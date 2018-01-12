280ddb2006e2038ac43a058e055dd5b9a16fede1
yaleman <yaleman@ricetek.net>
2018-01-13 07:33:09 +1000

    simple handling of coinbase API, waiting for key to enable

--- ledger.py
+++ ledger.py
@@ -2,9 +2,22 @@
 
 try:
 	import coinbase
+	import coinbase.wallet.client
 except ImportError:
 	exit("Please install coinbase package")
 
 import configparser
+
+
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+coinbase_client = coinbase.wallet.client.Client(config['coinbase']['api_key'], config['coinbase']['api_secret'])
+
+try:
+	coinbase_accounts = coinbase_client.get_accounts()
+except coinbase.wallet.error.AuthenticationError as e:
+	print("Authentication error contacting coinbase API, error:")
+	print(e)
+	exit()
+print(coinbase.accounts)
