75baf2236751e4ddb53eafa8dd5c59c61cf2ab34
yaleman <yaleman@ricetek.net>
2018-01-16 07:14:51 +1000

    coinbase works

--- ledger.py
+++ ledger.py
@@ -13,15 +13,17 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-coinbase_client = coinbase.wallet.client.Client(config['coinbase']['api_key'], config['coinbase']['api_secret'])
+def get_coinbase_accounts(config=config):
+	coinbase_client = coinbase.wallet.client.Client(config['coinbase']['api_key'], config['coinbase']['api_secret'])
 
-try:
-	coinbase_accounts = coinbase_client.get_accounts()
-except coinbase.wallet.error.AuthenticationError as e:
-	print("Authentication error contacting coinbase API, error:")
-	print(e)
-	exit()
-print(coinbase.accounts)
+	try:
+		coinbase_accounts = coinbase_client.get_accounts()
+	except coinbase.wallet.error.AuthenticationError as e:
+		print("Authentication error contacting coinbase API, error:")
+		print(e)
+		exit()
+	print(coinbase_accounts)
 
-
+#print( config['btcmarkets']['api_key_private'])
 # BTCMarkets has one, but it doesn't look great - https://github.com/BTCMarkets/api-client-python
+get_coinbase_accounts()
